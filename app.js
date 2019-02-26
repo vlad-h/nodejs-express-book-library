b7ba80abc2e5dfe9507e0fe03cf1aa782ecb9bda
vlad-h <vladm.hogas@gmail.com>
2019-02-26 21:59:48 +0200

    Authentification and authorization using passport

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,20 +15,29 @@ const nav = [
   { link: '/authors', title: 'Authors' },
 ];
 
-const bookRouter = require('./src/routes/bookRoutes')(nav);
-const adminRouter = require('./src/routes/adminRoutes')(nav);
-
 app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({ secret: 'library123', resave: true, saveUninitialized: true }));
+
+require('./src/config/passport.js')(app);
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.set('views', './src/views');
+
+const bookRouter = require('./src/routes/bookRoutes')(nav);
+const adminRouter = require('./src/routes/adminRoutes')(nav);
+const authRouter = require('./src/routes/authRoutes')(nav);
 /* If you want to test templating engine pug */
 // app.set('view engine', 'pug');
 app.set('view engine', 'ejs');
 
 app.use('/books', bookRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
 app.get('/', (req, res) => {
   /* Change to index-pug or index-ejs if you want to test pug or ejs templating engine */
   res.render('index', {
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -8,6 +8,13 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'libraryApp';
 
 function router(nav) {
+  bookRouter.use((req, res, next) => {
+    if (req.user) {
+      next();
+    } else {
+      res.redirect('/');
+    }
+  });
   bookRouter.route('/')
     .get((req, res) => {
       (async function mongo() {
